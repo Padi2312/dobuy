9a8f2a9d191fd446a5c619b7a7baef20ac4c0622
Hubbil <82642750+Hubbil@users.noreply.github.com>
2021-12-15 20:41:35 +0100

    design

--- php/pages/userprofile.php
+++ php/pages/userprofile.php
@@ -123,6 +123,66 @@
         //}
         ?>
         <button type="submit" id="dealer" action="">Produkte anbieten</button>
+        <h1>Für Adminseite oberes mit if-Bedingung nicht einblenden.</h1>
+        <div id="tableheadline">Übersicht aller Produkte</div>
+            <table id="table">
+                <tr>
+                    <th id="pictureadm">Bild</th>
+                    <th id="nameadm">Name</th>
+                    <th id="descriptionadm">Beschreibung</th>
+                    <th id="priceadm">Preis</th>
+                    <th id="edit">Bearbeiten</th>
+                </tr>
+                <tr>
+                    <td><img src="../../assets/images/logo.svg"></td>
+                    <td>Schuhe</td>
+                    <td>Schuhe halt</td>
+                    <td>187.69</td>
+                    <td class="outer"><a href=""><img src="../../assets/images/OOjs_UI_icon_edit-ltr.svg"></a></td>
+                </tr>
+                <tr>
+                    <td><img src="../../assets/images/logo.svg"></td>
+                    <td>Schuhe</td>
+                    <td>Schuhe halt</td>
+                    <td>187.69</td>
+                    <td class="outer"><a href=""><img src="../../assets/images/OOjs_UI_icon_edit-ltr.svg"></a></td>
+                </tr>
+                <tr>
+                    <td><img src="../../assets/images/logo.svg"></td>
+                    <td>Schuhe</td>
+                    <td>Schuhe halt</td>
+                    <td>187.69</td>
+                    <td class="outer"><a href=""><img src="../../assets/images/OOjs_UI_icon_edit-ltr.svg"></a></td>
+                </tr>
+            </table>
+            
+        <div id="tableheadline">Übersicht aller User</div>
+            <table id="table">
+                <tr>
+                    <th id="useradm">Username</th>
+                    <th id="usernameadm">Name</th>
+                    <th id="emailadm">E-Mail-Adresse</th>
+                    <th id="edit">Bearbeiten</th>
+                </tr>
+                <tr>
+                    <td>Gertalod</td>
+                    <td>Simon Lattin</td>
+                    <td>simon@lattin.de</td>
+                    <td class="outer"><a href=""><img src="../../assets/images/OOjs_UI_icon_edit-ltr.svg"></a></td>
+                </tr>
+                <tr>
+                    <td>Gertalod</td>
+                    <td>Simon Lattin</td>
+                    <td>simon@lattin.de</td>
+                    <td class="outer"><a href=""><img src="../../assets/images/OOjs_UI_icon_edit-ltr.svg"></a></td>
+                </tr>
+                <tr>
+                    <td>Gertalod</td>
+                    <td>Simon Lattin</td>
+                    <td>simon@lattin.de</td>
+                    <td class="outer"><a href=""><img src="../../assets/images/OOjs_UI_icon_edit-ltr.svg"></a></td>
+                </tr>
+            </table>  
     </main>
     <?php
     include "../templates/footer.php";
