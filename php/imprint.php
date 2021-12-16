b78443be776c6d91782da1eee2f02630562fe62c
simonlattin <sim.lattin.20@lehre.mosbach.dhbw.de>
2021-12-16 23:33:14 +0100

    corrected filestructure

---
+++ php/imprint.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <?php include_once '/common/session.php' ?>
+    <?php include_once '/templates/head.php' ?>
+    <link rel="stylesheet" href="../css/imprint.css">
+</head>
+
+<body>
+
+    <?php
+        include_once "/common/session.php";
+        include "/templates/header.php";
+    ?>
+
+    <main>
+
+    <span id="imprint">Impressum on Fleek</span>
+
+    </main>
+
+    <?php
+        include "/templates/footer.php";
+        include "/templates/scripts.php";
+    ?>
+</body>
+
+</html>
---
+++ php/privacy.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <?php include_once '/common/session.php' ?>
+    <?php include_once '/templates/head.php' ?>
+    <link rel="stylesheet" href="../css/privacy.css">
+</head>
+
+<body>
+
+    <?php
+        include_once "/common/session.php";
+        include "/templates/header.php";
+    ?>
+
+    <main>
+
+        <span id="privacy">Datenschutz on Fleek</span>
+
+    </main>
+    
+    <?php
+        include "/templates/footer.php";
+        include "/templates/scripts.php";
+    ?>
+</body>
+
+</html>
