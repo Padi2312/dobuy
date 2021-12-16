aca6aab23dd9a386a729b5069c449c4c626211e5
simonlattin <sim.lattin.20@lehre.mosbach.dhbw.de>
2021-12-16 22:40:00 +0100

    addproduct footer fix

--- php/pages/addproduct.php
+++ php/pages/addproduct.php
@@ -19,19 +19,17 @@
         
 
         <div class="submit-form">
-            <div class="form-container">
-                <form action="addproductaction.php" method="post" enctype="multipart/form-data">
-                    <label for="fileToUpload">Bild auswählen:</label></br>
-                    <input type="file" name="fileToUpload" id="fileToUpload" class="restricted"></br>
-                    <label for="productname">Wie soll das Produkt heißen?</label></br>
-                    <input type="text" name="productname" id="productname" class="bar"></br>
-                    <label for="price">Wie viel soll Ihr Produkt kosten?</label></br>
-                    <input type="number" step=".01" name="price" id="price" class="bar"></br>
-                    <label for="description">Beschreiben Sie Ihr Produkt</label></br>
-                    <textarea name="description" id="description" class="bar"></textarea></br>
-                    <button id="addproduct1">Produkt hinzufügen</button>
-                </form>
-            </div>
+            <form action="addproductaction.php" method="post" enctype="multipart/form-data">
+                <label for="fileToUpload">Bild auswählen:</label></br>
+                <input type="file" name="fileToUpload" id="fileToUpload" class="restricted"></br>
+                <label for="productname">Wie soll das Produkt heißen?</label></br>
+                <input type="text" name="productname" id="productname" class="bar"></br>
+                <label for="price">Wie viel soll Ihr Produkt kosten?</label></br>
+                <input type="number" step=".01" name="price" id="price" class="bar"></br>
+                <label for="description">Beschreiben Sie Ihr Produkt</label></br>
+                <textarea name="description" id="description" class="bar"></textarea></br>
+                <button id="addproduct1">Produkt hinzufügen</button>
+            </form>
         </div>
 
     </main>
