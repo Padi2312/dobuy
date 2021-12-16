05807650b1aa60e81df89353b6e262e238680f8c
simonlattin <sim.lattin.20@lehre.mosbach.dhbw.de>
2021-12-16 13:11:34 +0100

    register format

--- php/pages/register.php
+++ php/pages/register.php
@@ -12,27 +12,30 @@
 
     <main>
         <div id="border1" class="container-fluid ">
-            <form class="register-form" method="post" action="registeraction.php">
-                <div id="logintext"><img id="logo2" src="../../assets/images/logo.svg" title="Logo von DOBUY" alt="Logo von DOBUY"> </br>
-                    Registrieren Sie sich um unsere Inhalte vollständig nutzen zu können.</div>
+                <form class="register-form" method="post" action="registeraction.php">
+                    <div id="logintext"><img id="logo2" src="../../assets/images/logo.svg" title="Logo von DOBUY" alt="Logo von DOBUY"> </br>
+                        Registrieren Sie sich um unsere Inhalte vollständig nutzen zu können.
+                    </div>
+                    <hr id="seperator"/>
                     <div class="form-group">
-                    <input name="username" type="text" id="username" class="inputtext" required placeholder="Username">
+                        <input name="username" type="text" id="username" class="form-control" required placeholder="Username">
                     </div>
                     <div class="form-group">               
-                    <input name="firstname" type="text" id="firstname" class="inputtext" required placeholder="Vorname">
+                        <input name="firstname" type="text" id="firstname" class="form-control" required placeholder="Vorname">
                     </div>
                     <div class="form-group">
-                    <input name="lastname" type="text" id="lastname" class="inputtext" required placeholder="Nachname">
+                        <input name="lastname" type="text" id="lastname" class="form-control" required placeholder="Nachname">
                     </div>
                     <div class="form-group">
-                    <input name="email" type="text" id="email" class="inputtext" required placeholder="E-Mail">
+                        <input name="email" type="text" id="email" class="form-control" required placeholder="E-Mail">
                     </div>
                     <div class="form-group">
-                    <input name="password" type="password" id="password" class="inputtext" required placeholder="Password">
+                        <input name="password" type="password" id="password" class="form-control" required placeholder="Password">
                     </div>
-                    <button type="submit" name="register" id="reg">Registrieren</button>
-                    
-            </form>
+                    <div id="buttonline">
+                        <button type="submit" name="register" id="reg" class="btn">Registrieren</button>
+                    </div>
+                </form>
         </div>
 
     </main>
