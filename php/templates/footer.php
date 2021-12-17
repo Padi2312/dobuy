2de2b53e681807277b4d0db79be6822bffe0bc44
padi2312 <patrick.arndt.99@web.de>
2021-12-17 01:28:28 +0100

    Fancy footer back in game

--- php/templates/footer.php
+++ php/templates/footer.php
@@ -1,5 +1,5 @@
 <footer class="container-footer-wrapper">
-    <div class="container">
+    <!-- <div class="container">
         <div class="row">
             <div id="footer-link-div" class="col-sm">
                 <a href="../../php/pages/imprint.php" class="footer-link"><span id="link">Impressum</span></a>
@@ -11,5 +11,52 @@
                 <a href="../../php/pages/contact.php" class="footer-link"><span id="link">Kontakt</span></a>
             </div>
         </div>
+    </div> -->
+    <div class="container-fluid">
+        <div>
+            <div class="row ml-5">
+                <div class="col-md-4 mb-4">
+                    <h2>Kontaktinfos</h2>
+                    <div class="box-1_inner_wrap">
+                        <span>Kundenhotline</span><br>
+                        <a href="tel:+49123456" class="footer_tel" title="Die Telefonnummer 0123456789 anrufen">01234 5678 - 9</a>
+                    </div>
+                    <div class="box-1_inner_wrap">
+                        <span>Kontaktmail</span><br>
+                        <a href="mailto:max@musterman.com" class="footer_mail" title="Fenster zum schreiben der E-mail öffnen">E-Mail schreiben</a>
+                    </div>
+                </div>
+                <div class="col-md-5 mb-4">
+                    <h2>Geschäftszeiten</h2>
+                    <div class="box-2_inner-wrap">
+                        <b class="day">Mo-Do:</b>
+                        <span class="times">8 - 16 Uhr</span>
+                    </div>
+                    <div class="box-2_inner-wrap">
+                        <b class="day">Fr:</b>
+                        <span class="times">10 - 14 Uhr</span>
+                    </div>
+                    <div class="box-2_inner-wrap">
+                        <b class="day">Sa:</b>
+                        <span class="times">10 - 12 Uhr</span>
+                    </div>
+                </div>
+                <div class="col-md-3 mb-4">
+                    <h2>Weitere Links</h2>
+                    <ul class="box-3_inner_wrap">
+                        <li>
+                            <a href="../../php/pages/imprint.php" class="footer-link"><span id="link">Impressum</span></a>
+                        </li>
+                        <li>
+                            <a href="../../php/pages/datasecurity.php" class="footer-link"><span id="link">Datenschutz</span></a>
+                        </li>
+                        <li>
+                            <a href="../../php/pages/contact.php" class="footer-link"><span id="link">Kontakt</span></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
+
 </footer>
